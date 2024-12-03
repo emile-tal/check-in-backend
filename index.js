f75930e7e3cc16bbc047108b99e76927bc75ef87
Emile Tal <emiletal@gmail.com>
2024-12-03 18:01:10 -0500

    Setup register and login user with JWT token

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import userRoutes from './routes/user_routes'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8081;
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/user', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
---
+++ routes/user_routes.js
@@ -0,0 +1,78 @@
+import bcrypt from 'bcrypt'
+import configuration from '../knexfile.js'
+import express from 'express'
+import initKnex from 'knex'
+import jwt from 'jsonwebtoken'
+
+const saltRounds = 10
+
+const knex = initKnex(configuration)
+const router = express.Router()
+
+const hashPass = async (password) => {
+    try {
+        const hashedPass = await bcrypt.hash(password, saltRounds)
+        return hashedPass
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const checkPass = async (password, dbPass) => {
+    try {
+        const match = await bcrypt.compare(password, dbPass)
+        return match
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+router.route('/login')
+    .post(async (req, res) => {
+        const { username, password } = req.body
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password is required" })
+        }
+
+        try {
+            const user = await knex('user').where({ username: username }).first()
+            if (!user) {
+                return res.status(401).json({ message: "Username is incorrect" })
+            }
+            if (!checkPass(password, user.password)) {
+                return res.status(401).json({ message: "Password is incorrect" })
+            }
+            const jwtToken = jwt.sign(
+                { id: user.id, name: user.username }, JWT_SECRET
+            )
+            res.status(200).json({ message: "Login successful", token: jwtToken })
+        } catch (error) {
+            res.status(500).json({ message: 'Unable to process login' })
+        }
+    })
+
+router.route('/register')
+    .post(async (req, res) => {
+        const { username, password, email } = req.body
+
+        if (!username || !password || !email) {
+            return res.status(400).json({ message: "Username and password is required" })
+        }
+        try {
+            const hashedPass = hashPass(password)
+            const [newUserId] = await knex('user').insert({
+                username,
+                hashedPass,
+                email
+            })
+            const jwtToken = jwt.sign(
+                { id: newUserId, name: username }, JWT_SECRET
+            )
+            res.status(201).json({ message: "User created successfully", token: jwtToken })
+        } catch (error) {
+            res.status(500).json({ message: 'Unable to process login' })
+        }
+    })
+
+export default router
